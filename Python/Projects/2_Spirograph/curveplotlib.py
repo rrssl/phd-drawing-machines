42e769c7cd3373298187b5dec06fc74c9fa96b4b
Robin Roussel <robin.roussel@inria.fr>
2016-03-30 20:40:43 +0200

    Minor

--- Python/Projects/2_Spirograph/curveplotlib.py
+++ Python/Projects/2_Spirograph/curveplotlib.py
@@ -13,14 +13,17 @@ except ImportError:
     CV2_IMPORTED = False
 else:
     CV2_IMPORTED = True
-    
+
 import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
 
 def plot(curve, *args, **kwargs):
+    """Plot the input curve."""
     plt.plot(curve[0], curve[1], *args, **kwargs)
 
 class PixelFormatter:
+    """Coordinate formatter to show pixel value with pyplot.imshow."""
+
     def __init__(self, im):
         self.im = im
     def __call__(self, x, y):
@@ -31,27 +34,32 @@ class PixelFormatter:
         return 'x={:.01f}, y={:.01f}, z={:.01f}'.format(x, y, z)
 
 def imshow(img, curve=None, viewer='plt'):
+    """Show a raster image, optionnally along with a superimposed curve."""
     if viewer == 'plt':
-        # Display using Pyplot.    
+        # Display using Pyplot.
         if curve is not None:
             plot(curve)
-            
-        shp = img.shape        
+
+        shp = img.shape
         if len(shp) == 2:
-            pltim = plt.imshow(img, interpolation='none', cmap=plt.cm.gray)
+            pltim = plt.imshow(img, interpolation=None, cmap=plt.cm.gray)
         elif len(shp) == 3 and shp[2] == 3:
             # /!\ OpenCV uses BGR order, while Pyplot uses RGB!
             img2 = img[:,:,::-1]
             pltim = plt.imshow(img2, interpolation='none')
-        
+
+        frame = plt.gca()
         # Show pixel value when hovering over it.
-        plt.gca().format_coord = PixelFormatter(pltim)
-        
+        frame.format_coord = PixelFormatter(pltim)
+        # Remove axis text.
+        frame.axes.get_xaxis().set_ticks([])
+        frame.axes.get_yaxis().set_ticks([])
+
     elif viewer == 'qt':
         if CV2_IMPORTED:
             # Display using OpenCV's Qt viewer.
             cv2.namedWindow('Viewer',  cv2.WINDOW_NORMAL)
-#            img2 = cv2.normalize(img, 0, 255, norm_type=cv2.NORM_MINMAX, 
+#            img2 = cv2.normalize(img, 0, 255, norm_type=cv2.NORM_MINMAX,
 #                                 dtype=cv2.CV_8UC1)
             img2 = cv2.normalize(img, 0, 1, norm_type=cv2.NORM_MINMAX)
             cv2.imshow('Viewer', img2)
@@ -59,12 +67,12 @@ def imshow(img, curve=None, viewer='plt'):
             cv2.destroyAllWindows()
         else:
             print("Qt viewer cannot be used: OpenCV module not found.")
-        
+
 def cvtshow(curve, curvature):
-    # Plot curvature.
+    """Plot the curvature along the input curve."""
     points = curve.T.reshape(-1, 1, 2)
     segments = np.concatenate([points[:-1], points[1:]], axis=1)
-    
+
     lc = LineCollection(segments, cmap=plt.cm.winter)
     lc.set_array(curvature)
     lc.set_linewidth(2)
@@ -72,4 +80,4 @@ def cvtshow(curve, curvature):
     plt.gca().set_aspect('equal')
     plt.gca().add_collection(lc)
     plt.autoscale()
-    plt.margins(0.1)  
+    plt.margins(0.1)
