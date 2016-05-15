1319684a382368347435e15e59c155d6e58e47e5
Robin Roussel <robin.roussel@inria.fr>
2016-05-15 19:13:35 +0200

    Reorganize RoulettePlot + add automatic boundary update.

--- Python/Projects/2_Spirograph/spiroplots.py
+++ Python/Projects/2_Spirograph/spiroplots.py
@@ -42,10 +42,11 @@ class SpiroPlot:
         plt.subplots_adjust(bottom=0.25)
 #        ax = fig.add_subplot(111)
 
-        axcolor = 'lightgoldenrodyellow'
-        self.sliderax_R = plt.axes([0.15, 0.05, 0.65, 0.03], axisbg=axcolor)
-        self.sliderax_r = plt.axes([0.15, 0.1, 0.65, 0.03], axisbg=axcolor)
-        self.sliderax_d = plt.axes([0.15, 0.15, 0.65, 0.03], axisbg=axcolor)
+        self.sliderax_R = plt.axes([0.15, 0.05, 0.65, 0.03], axisbg='pink')
+        self.sliderax_r = plt.axes([0.15, 0.1, 0.65, 0.03],
+                                   axisbg='lightgreen')
+        self.sliderax_d = plt.axes([0.15, 0.15, 0.65, 0.03],
+                                   axisbg='lightgreen')
 
         self.s_R = DiscreteSlider(self.sliderax_R, 'R', self.R_min_val,
                                   self.R_max_val, allowed_vals=self.R_vals,
@@ -162,59 +163,74 @@ class SpiroGridPlot:
         curve = curve + position.reshape(2, 1)
 
         plt.plot(curve[0], curve[1], 'b-')
-        
-        
+
+
+# TODO: rather than updating the boundaries by changing the slider's scale, 
+# add another slider in the same slideraxes symbolizing the upper limit.
 class RoulettePlot:
     """Forward geometric simulation of a roulette curve (ellipse in circle)."""
-    
+    initial_parameters = {
+        'R': {'valmin': 1,    # Radius of the outer circle.
+              'valmax': 15,
+              'valinit': 8},
+        'S': {'valmin': 0,    # Uniform scale factor of the ellipse.
+              'valmax': None,
+              'valinit': None},
+        'e': {'valmin': 0.,   # Eccentricity of the ellipse (0 < e < 1).
+              'valmax': 1.,
+              'valinit': 0.5},
+        'd': {'valmin': 0.,   # Radius of the tracing point in the ellipse.
+              'valmax': None,
+              'valinit': None}
+         }
+
     def __init__(self, show_gears=True):
-        # Radius of the outer circle.
-        self.R_min_val = 1
-        self.R_max_val = 15 # included
-        self.R_vals = np.arange(self.R_min_val, self.R_max_val + 1, 1)
-        self.R = 8
-        # Eccentricity of the ellipse (0 < e < 1)
-        self.e_min_val = 0. # included (to be able to get the circle case)
-        self.e_max_val = 1. # excluded
-        self.e = 0.5
+        params = RoulettePlot.initial_parameters
+
+        R_vals = np.arange(params['R']['valmin'], params['R']['valmax'] + 1, 1)
+        self.R = params['R']['valinit']
+        self.e = params['e']['valinit']
+
+        # Deduce the reference values of the semiaxes.
         e2 = self.e * self.e
-        # We can deduce the reference value of semimajor axis, a0 = pi / 2E(e),
-        # as well as b0 = a0 * sqrt(1 - e**2)
-        # and the maximum bound of the scale max_s = R * sqrt(1 - e**2) / a0.
         self.a0 = np.pi / (2 * spec.ellipe(e2))
-        self.b0 = self.a0 * np.sqrt(1 - e2)        
-        # S factor of the ellipse.
-        self.S_min_val = 0
-        self.S_max_val = int(self.get_S_max_val()) # included
-        self.S_vals = np.arange(self.S_min_val, self.S_max_val + 1, 1)
-        self.S = 2
-        # We can deduce the auxiliary parameters.
-        self.a = self.S * self.a0        
+        self.b0 = self.a0 * np.sqrt(1 - e2)
+
+        # Deduce the missing values for S.
+        if params['S']['valmax'] is None:
+            params['S']['valmax'] = int(self.get_S_max_val())
+        S_vals = np.arange(params['S']['valmin'], params['S']['valmax'] + 1, 1)
+        if params['S']['valinit'] is None:
+            params['S']['valinit'] = int(
+                (params['S']['valmin'] + params['S']['valmax']) / 2)
+        self.S = params['S']['valinit']
+
+        # Deduce the current values of the semiaxes.
+        self.a = self.S * self.a0
         self.b = self.S * self.b0
-        # Distance from the center of the ellipse to the tracing point.
-        self.d_min_val = 0.
-        self.d_max_val = self.a # included
-        self.d = 0.5
 
+        # Deduce the missing values for d.
+        if params['d']['valmax'] is None:
+            params['d']['valmax'] = self.a
+        if params['d']['valinit'] is None:
+            params['d']['valinit'] = (
+                params['d']['valmin'] + params['d']['valmax']) / 2
+        self.d = params['d']['valinit']
+
+        # Create the sliders.
         self.fig, self.ax = plt.subplots()
-        plt.subplots_adjust(bottom=0.3)
+        plt.subplots_adjust(bottom=0.1 + 0.05 * len(params))
+        sliderax_R = plt.axes([0.15, 0.2, 0.65, 0.03], axisbg='pink')
+        sliderax_S = plt.axes([0.15, 0.15, 0.65, 0.03], axisbg='lightgreen')
+        sliderax_e = plt.axes([0.15, 0.1, 0.65, 0.03], axisbg='lightgreen')
+        sliderax_d = plt.axes([0.15, 0.05, 0.65, 0.03], axisbg='lightgreen')
 
-        axcolor = 'lightgoldenrodyellow'
-        self.sliderax_R = plt.axes([0.15, 0.2, 0.65, 0.03], axisbg=axcolor)
-        self.sliderax_S = plt.axes([0.15, 0.15, 0.65, 0.03], axisbg=axcolor)
-        self.sliderax_e = plt.axes([0.15, 0.1, 0.65, 0.03], axisbg=axcolor)
-        self.sliderax_d = plt.axes([0.15, 0.05, 0.65, 0.03], axisbg=axcolor)
-
-        self.s_R = DiscreteSlider(self.sliderax_R, 'R', self.R_min_val,
-                                  self.R_max_val, allowed_vals=self.R_vals,
-                                  valinit=self.R)
-        self.s_S = DiscreteSlider(self.sliderax_S, 'S', self.S_min_val, 
-                                  self.S_max_val, allowed_vals=self.S_vals,
-                                  valinit=self.S)
-        self.s_e = Slider(self.sliderax_e, 'e', self.e_min_val, self.e_max_val,
-                          valinit=self.e, closedmax=False)
-        self.s_d = Slider(self.sliderax_d, 'd', self.d_min_val, self.d_max_val,
-                          valinit=self.d)
+        self.s_R = DiscreteSlider(sliderax_R, 'R', allowed_vals=R_vals,
+                                  **params['R'])
+        self.s_S = DiscreteSlider(sliderax_S, 'S', allowed_vals=S_vals,
+                                  **params['S'])
+        self.s_e = Slider(sliderax_e, 'e', closedmax=False, **params['e'])
+        self.s_d = Slider(sliderax_d, 'd', **params['d'])
 
         self.s_R.on_changed(self.update_R)
         self.s_S.on_changed(self.update_S)
@@ -234,78 +250,122 @@ class RoulettePlot:
         b = self.b
         ax = self.ax
 
-        if not S:
-            return
-            
-        if update_tracer_only:
-            curve = self.roulette.update_tracer(self.d)
-        else:
-            N = Fraction.from_float(R / S).limit_denominator(100).denominator
-            t = np.linspace(0., N * 2 * np.pi, N * 300)
-
-            self.roulette = RouletteEllipseInCircle(R, a, b, d)
-            curve = self.roulette.get_point(t)
-
-        if self.show_gears:
-            out_gear = pat.Circle((0., 0.), R, color='r', fill=False)
-            int_gear = pat.Ellipse((R - a, 0.), 2 * a, 2 * b, color='g', 
-                                   fill=False)
-            hole = pat.Circle((R - a + d, 0.), R / 40, color='g', fill=False)
-
         ax.cla()
         ax.set_aspect('equal')
         ax.grid(True)
 
-        if self.show_gears:
-            ax.add_artist(out_gear)
-            ax.add_artist(int_gear)
-            ax.add_artist(hole)
-        ax.plot(curve[0], curve[1])
+        if S:
+            if update_tracer_only:
+                curve = self.roulette.update_tracer(self.d)
+            else:
+                N = Fraction.from_float(R / S).limit_denominator(100).denominator
+                t = np.linspace(0., N * 2 * np.pi, N * 100)
 
-        dim = max(R - a + d, R) + 1
-        ax.set_xlim(-dim, dim)
-        ax.set_ylim(-dim, dim)
+                self.roulette = RouletteEllipseInCircle(R, a, b, d)
+                curve = self.roulette.get_point(t)
+
+            if self.show_gears:
+                out_gear = pat.Circle((0., 0.), R, color='r', fill=False)
+                int_gear = pat.Ellipse((R - a, 0.), 2 * a, 2 * b, color='g',
+                                       fill=False)
+                hole = pat.Circle((R - a + d, 0.), R / 40, color='g',
+                                  fill=False)
+
+            if self.show_gears:
+                ax.add_artist(out_gear)
+                ax.add_artist(int_gear)
+                ax.add_artist(hole)
+            ax.plot(curve[0], curve[1])
+
+            dim = max(R - a + d, R) + 1
+            ax.set_xlim(-dim, dim)
+            ax.set_ylim(-dim, dim)
+        else:
+            if self.show_gears:
+                out_gear = pat.Circle((0., 0.), R, color='r', fill=False)
+                ax.add_artist(out_gear)
+
+                dim = R + 1
+                ax.set_xlim(-dim, dim)
+                ax.set_ylim(-dim, dim)
 
         self.fig.canvas.draw()
-    
-    def update_R(self, value):
-        """Callback function for parameter 'R' update."""
-        self.R = self.s_R.val
-        # Update the upper bound of S if necessary.
-        S_max_val = self.get_S_max_val()
-        if self.S >= S_max_val:
-            self.s_S.set_val(int(S_max_val))            
+
+    def update(self, parameter):
+        """Callback function for parameter update.
+
+        We use a single function to avoid multiple calls to self.draw().
+        """
+        if parameter == 'd':
+            self.d = self.s_d.val
+            self.draw(update_tracer_only=True)
         else:
-            self.draw()
-        
-    def update_e(self, value):
-        """Callback function for parameter 'e' update."""
-        self.e = self.s_e.val
-        e2 = self.e * self.e
-        self.a0 = np.pi / (2 * spec.ellipe(e2))
-        self.b0 = self.a0 * np.sqrt(1 - e2)
-        # Update the upper bound of S if necessary.
-        S_max_val = self.get_S_max_val()
-        if self.S >= S_max_val:
-            self.s_S.set_val(int(S_max_val)) 
-        else:    
+            if parameter == 'S':
+                self.S = self.s_S.val
+            elif parameter == 'e':
+                self.e = self.s_e.val
+                e2 = self.e * self.e
+                self.a0 = np.pi / (2 * spec.ellipe(e2))
+                self.b0 = self.a0 * np.sqrt(1 - e2)
+            elif parameter == 'R':
+                self.R = self.s_R.val
+            else:
+                return
+
+            # Temporarily freeze the widget callbacks.
+            self.s_S.eventson = False
+            self.s_S.drawon = False
+            self.s_d.eventson = False
+            self.s_d.drawon = False
+
+            # Update the upper bound of S if necessary.
+            S_max_val = self.get_S_max_val()
+            S_max_val = int(S_max_val) - (1 if S_max_val.is_integer() else 0)
+            if self.s_S.valmax != S_max_val:
+                self.s_S.valmax = S_max_val
+                if S_max_val:
+                    self.s_S.ax.set_xlim(right=S_max_val)
+                if self.S > S_max_val:
+                    self.s_S.set_val(S_max_val)
+                    self.S = S_max_val
+
             self.a = self.S * self.a0
             self.b = self.S * self.b0
+
+            # Update the upper bound of d if necessary.
+            self.s_d.valmax = self.a
+            if self.a:
+                self.s_d.ax.set_xlim(right=self.a)
+            if self.d > self.a:
+                self.s_d.set_val(self.a)
+                self.d = self.a
+
+            # Un-freeze the widgets callbacks.
+            self.s_S.eventson = True
+            self.s_S.drawon = True
+            self.s_d.eventson = True
+            self.s_d.drawon = True
+
             self.draw()
+
+    def update_R(self, value, redraw=True):
+        """Callback function for parameter 'R' update."""
+        self.update('R')
+
+    def update_e(self, value, redraw=True):
+        """Callback function for parameter 'e' update."""
+        self.update('e')
 
     def update_S(self, value):
         """Callback function for parameter 'S' update."""
-        self.S = self.s_S.val
-        self.a = self.S * self.a0
-        self.b = self.S * self.b0
-        self.draw()
+        self.update('S')
 
     def update_d(self, value):
         """Callback function for parameter 'd' update."""
-        self.d = self.s_d.val
-        self.draw(update_tracer_only=True)
-        
+        self.update('d')
+
     def get_S_max_val(self):
+        """Get the maximum S value so that the curvature constraint is met."""
         return self.R * np.sqrt(1 - self.e ** 2) / self.a0
 
 
