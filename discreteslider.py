7e46cf151354bca58c428c67d5787631f0f68aae
Robin Roussel <robin.roussel@inria.fr>
2016-09-02 12:35:05 +0200

    Minor bug

--- discreteslider.py
+++ discreteslider.py
@@ -7,6 +7,7 @@ Specialization of matplotlib's Slider widget to take discrete values.
 https://stackoverflow.com/questions/23703105/discrete-slider-in-matplotlib-widget
 """
 from matplotlib.widgets import Slider
+from numpy import float_
 
 class DiscreteSlider(Slider):
     """A matplotlib slider widget with discrete steps."""
@@ -24,6 +25,11 @@ class DiscreteSlider(Slider):
             self.allowed_vals = [self.valmin,self.valmax]
 
     def set_val(self, val):
+        # If 'valmin' or 'valmax' is given as an int, it will be passed as an
+        # int when the user reaches an extremity of the slider, causing an
+        # error with 'val - self.allowed_vals'.
+        val = float_(val)
+
         discrete_val = self.allowed_vals[abs(val - self.allowed_vals).argmin()]
         xy = self.poly.xy
         xy[2] = discrete_val, 1
