9b8373de3b25d413c3f0f57624f545dce425be64
Robin Roussel <robin.roussel@inria.fr>
2016-09-02 12:33:57 +0200

    Move mecha.py + Complete SingleGearFixedFulcrumCDM code + minor.

--- mecha.py
+++ mecha.py
@@ -5,12 +5,12 @@ Parametrized cyclic mechanisms
 @author: Robin Roussel
 """
 
+from fractions import gcd
 import math
 import numpy as np
 import scipy.optimize as opt
 import scipy.special as spec
 
-import context
 from utils import skipends, farey
 import curves as cu
 
@@ -48,7 +48,8 @@ class Mechanism:
             """Check that the input properties comply with the constraints."""
             result = all(cons(props) >= 0 for cons in cls.get_constraints())
             if not result:
-                print([cons(props) for cons in cls.get_constraints()])
+                print("Constraints: ",
+                      [cons(props) for cons in cls.get_constraints()])
             return all(cons(props) >= 0 for cons in cls.get_constraints())
 
         @classmethod
@@ -100,17 +101,23 @@ class Mechanism:
             print("Error: invalid mechanism properties.", props)
 
     def update_prop(self, pid, value, check=True):
-        """Update the property referenced by the input index."""
-        assert(0 < pid < len(self.props))
+        """Update the property referenced by the input index.
+
+        Returns True if the new value complies with the constraints, False
+        otherwise.
+        """
+        assert(0 <= pid < len(self.props))
 
         oldval = self.props[pid]
         self.props[pid] = value
         if (not check
             or self.constraint_solver.check_constraints(self.props)):
             self._simulator.update_prop(pid, value)
+            return True
         else:
             self.props[pid] = oldval
             print("Error: invalid mechanism properties.", pid, value)
+            return False
 
     def get_prop_bounds(self, pid):
         """Get the bounds of the property referenced by the input index."""
@@ -175,7 +182,7 @@ class BaseSpirograph(DrawingMechanism):
             if pid == 0:
                 return prop[1] + 1, cls.max_nb_turns
             elif pid == 1:
-                return 0, prop[0] - 1
+                return 1, prop[0] - 1
             else:
                 return 0., prop[1] - 2*cls.eps
 
@@ -277,8 +284,7 @@ class EllipticSpirograph(DrawingMechanism):
                     lambda p: cls.max_nb_turns - p[0],
                     lambda p: p[0] - p[1] - 1,
                     lambda p: p[2],
-                    lambda p: cls._get_e2max(
-                        p[0], p[1]) - p[2] - cls.eps,
+                    lambda p: cls._get_e2max(p[0], p[1]) - p[2] - cls.eps,
                     lambda p: p[3],
                     lambda p: cls._get_dmax(p[1], p[2]) - p[3] - cls.eps
                     )
@@ -291,7 +297,7 @@ class EllipticSpirograph(DrawingMechanism):
             if pid == 0:
                 return prop[1] + 1, cls.max_nb_turns
             elif pid == 1:
-                return 0, prop[0] - 1
+                return 1, prop[0] - 1
             elif pid == 2:
                 return 0., cls._get_e2max(prop[0], prop[1]) - 2*cls.eps
             else:
@@ -335,7 +341,7 @@ class EllipticSpirograph(DrawingMechanism):
 
         def __init__(self, properties, nb_samples=2**6, per_turn=True):
             self.roul = None
-            self.reset(properties, nb_samples)
+            self.reset(properties, nb_samples, per_turn)
 
         def reset(self, properties, nb_samples=2**6, per_turn=True):
             """Reset the class fields."""
@@ -417,22 +423,87 @@ class SingleGearFixedFulcrumCDM(DrawingMechanism):
 
     class ConstraintSolver(Mechanism.ConstraintSolver):
         """Class for handling design constraints."""
-        max_nb_turns = 20 # Arbitrary value
+        max_nb_turns = 25 # Arbitrary value
 
         @classmethod
         def get_constraints(cls, cstr={}):
             """Get the constraints data."""
-            raise NotImplementedError
+            try:
+                return cstr[cls]
+            except KeyError:
+                cstr[cls] = (
+                    lambda p: p[0] - 1,                         # R_t > 0
+                    lambda p: cls.max_nb_turns - p[0],          # R_t <= B
+                    lambda p: p[1] - 1,                         # R_g > 0
+                    lambda p: cls.max_nb_turns - p[1],          # R_g <= B
+                    lambda p: p[2] - p[0] - cls.eps,            # d_f > R_t
+                    lambda p: 2*cls.max_nb_turns - p[2],        # d_f <= 2B
+                    lambda p: p[3],                             # theta_g >= 0
+                    lambda p: math.pi - p[3],                   # theta_g <= pi
+                    lambda p: p[4] - cls.eps,                   # d_p > 0
+                    lambda p: cls._get_FG(*p[:4]) - p[1] - p[4] - cls.eps,
+                                                                # d_p < FG-R_g
+                    lambda p: p[5],                             # d_s >= 0
+                    lambda p: p[1] - p[5] - cls.eps,            # d_s < R_g
+                    lambda p: p[0] - cls._get_OP_max(*p)        # R_t >= OP_max
+                    )
+                return cstr[cls]
 
         @classmethod
         def get_bounds(cls, prop, pid):
             """Get the bounds of the property prop[pid]."""
-            raise NotImplementedError
+            assert(0 <= pid < len(prop))
+            prop = list(prop)
+            def adapt(cstr):
+                return lambda x: cstr(prop[:pid] + [x] + prop[pid+1:])
+
+            cstrs = cls.get_constraints()
+            if pid == 0:
+                ids = [0, 1, 4, 9, -1]
+            elif pid == 1:
+                ids = [2, 3, 9, -2, -1]
+            elif pid == 2:
+                ids = [4, 5, 9, -1]
+            elif pid == 3:
+                ids = [6, 7, 9, -1]
+            elif pid == 4:
+                ids = [8, 9, -1]
+            else:
+                ids = [10, 11, -1]
+            cstrs = [adapt(cstrs[i]) for i in ids]
+
+            min_ = opt.fmin_cobyla(
+                lambda x: x, prop[pid], cons=cstrs, disp=0)
+            max_ = opt.fmin_cobyla(
+                lambda x: -x, prop[pid], cons=cstrs, disp=0)
+
+            if pid == 0 or pid == 1:
+                min_ = math.ceil(min_)
+                max_ = math.floor(max_)
+
+            return min_, max_
 
         @classmethod
         def sample_feasible_domain(cls, grid_resol):
             """Sample the feasible domain."""
             return None
+
+        @staticmethod
+        def _get_FG(R_t, R_g, d_f, theta_g):
+            """Get the distance between the fulcrum and the gear center."""
+            OG =  R_t + R_g
+            return math.sqrt(d_f**2 + OG**2 - 2*d_f*OG*math.cos(theta_g))
+
+        @classmethod
+        def _get_OP_max(cls, R_t, R_g, d_f, theta_g, d_p, d_s):
+            """Get the maximum distance between the center and the penholder."""
+            OG =  R_t + R_g
+            FG = cls._get_FG(R_t, R_g, d_f, theta_g)
+            alpha = math.atan2(d_s, FG)
+            theta_fg = math.pi - math.atan2(OG*math.sin(theta_g),
+                                            d_f - OG*math.cos(theta_g))
+            return math.sqrt(
+                d_f**2 + d_p**2 + 2*d_f*d_p*math.cos(theta_fg-alpha))
 
 
     class Simulator(Mechanism.Simulator):
@@ -445,22 +516,24 @@ class SingleGearFixedFulcrumCDM(DrawingMechanism):
             """Reset the properties."""
             self.R_t = properties[0] # Turntable radius
             self.R_g = properties[1] # Gear radius
-            self.d_f = properties[2] # Fulcrum-center distance
+            self.C_f = np.array([properties[2], 0.]) # Fulcrum center
             self.theta_g = properties[3] # Gear polar angle
             self.d_p = properties[4] # Fulcrum-penholder distance
             self.d_s = properties[5] # Gear center - slider distance
             # Gear center
-            self.C_g = (self.R_t + self.R_g) * np.array([math.cos(self.theta_g),
-                                                         math.sin(self.theta_g)])
-            # Fulcrum center
-            self.C_f = np.array([self.d_f, 0.])
+            self.C_g = (self.R_t + self.R_g) * np.array(
+                [math.cos(self.theta_g), math.sin(self.theta_g)])
 
             self.nb_samples = nb_samples
             self.per_turn = per_turn
 
         def simulate_cycle(self):
             """Simulate one cycle of the assembly's motion."""
-            nb_turns = self.R_t
+            gcd_ = gcd(self.R_t, self.R_g)
+            nb_turns = self.R_t / gcd_
+            if not self.d_s:
+                # Degenerate case.
+                nb_turns /= self.R_g / gcd_
             if self.per_turn:
                 nb_samples = nb_turns * self.nb_samples + 1
             else:
@@ -484,3 +557,47 @@ class SingleGearFixedFulcrumCDM(DrawingMechanism):
             curve = np.einsum('ijk,jk->ik', rot, curve)
 
             return curve
+
+        def update_prop(self, pid, value):
+            """Update the property referenced by the input index."""
+            assert (0 <= pid < 6)
+            if pid == 0:
+                self.R_t = value
+            elif pid == 1:
+                self.R_g = value
+            elif pid == 2:
+                self.C_f[0] = value
+            elif pid == 3:
+                self.theta_g = value
+            elif pid == 4:
+                self.d_p = value
+            else:
+                self.d_s = value
+            if pid == 0 or pid == 1 or pid == 3:
+                self.C_g = (self.R_t + self.R_g) * np.array(
+                    [math.cos(self.theta_g), math.sin(self.theta_g)])
+
+    def get_curve(self, nb=2**6, per_turn=True):
+        """Return an array of points sampled on the full curve.
+
+        By default the number of points is not absolute, but rather "per turn"
+        of the input driver. In this case, powers of 2 can make the
+        computation faster.
+        The curve parameter is evenly sampled, but the curve points are not
+        necessarily evenly spaced.
+        """
+        self._simulator.nb_samples = nb
+        self._simulator.per_turn = per_turn
+        return self._simulator.simulate_cycle()
+
+    def get_point(self, t):
+        """Return a specific curve point."""
+        raise NotImplementedError
+
+    def get_arc(self, start, stop, nb):
+        """Return 'nb' curve points evenly sampled on a parameter interval.
+
+        The curve parameter is evenly sampled, but the curve points are not
+        necessarily evenly spaced.
+        """
+        raise NotImplementedError
