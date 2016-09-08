3d66996311b6458dba9831f2dd6800e6fa5b92b9
Robin Roussel <robin.roussel@inria.fr>
2016-09-08 11:40:25 +0200

    Add the elliptical involute gear profile.

--- examples/gears.py
+++ examples/gears.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as pat
 
 import context
-from gearprofile import Involute, Sinusoidal, Cycloidal, SinusoidalElliptic
+import gearprofile as gear
 from curves import Ellipse2
 
 
@@ -26,81 +26,102 @@ def main():
                                      linestyle='dashed', fill=False)
 
     # First gear type: involute
-    ax = plt.subplot(221)
-    gearext = Involute(radext, radial_pitch*radext, internal=True)
+    ax = plt.subplot(231)
+    gearext = gear.Involute(radext, radial_pitch*radext, internal=True)
     profext = gearext.get_profile()
-    plt.plot(profext[0], profext[1])
+    plt.plot(*profext)
 
-    gearint = Involute(radint, radial_pitch*radint)
+    gearint = gear.Involute(radint, radial_pitch*radint)
     profint = gearint.get_profile()
     profint[0] += radext - radint
-    plt.plot(profint[0], profint[1])
+    plt.plot(*profint)
 
     ax.set_aspect('equal')
+    ax.set_title("Involute")
     ax.add_patch(get_primext())
     ax.add_patch(get_primint())
 
 
     # Second gear type: sinusoidal
-    ax = plt.subplot(222)
-    gearext = Sinusoidal(radext, radial_pitch*radext, tooth_radius=0.1)
+    ax = plt.subplot(232)
+    gearext = gear.Sinusoidal(radext, radial_pitch*radext, tooth_radius=0.1)
     profext = gearext.get_profile()
-    plt.plot(profext[0], profext[1])
+    plt.plot(*profext)
 
-    gearint = Sinusoidal(radint, radial_pitch*radint, tooth_radius=0.1)
+    gearint = gear.Sinusoidal(radint, radial_pitch*radint, tooth_radius=0.1)
     profint = gearint.get_profile()
     profint[0] += radext - radint
-    plt.plot(profint[0], profint[1])
+    plt.plot(*profint)
 
     ax.set_aspect('equal')
+    ax.set_title("Sinusoidal")
     ax.add_patch(get_primext())
     ax.add_patch(get_primint())
 
 
     # Third gear type: cycloidal
-    ax = plt.subplot(223)
-    gearext = Cycloidal(radext, radial_pitch*radext)
+    ax = plt.subplot(233)
+    gearext = gear.Cycloidal(radext, radial_pitch*radext)
     profext = gearext.get_profile()
-    plt.plot(profext[0], profext[1])
+    plt.plot(*profext)
 
-    gearint = Cycloidal(radint, radial_pitch*radint)
+    gearint = gear.Cycloidal(radint, radial_pitch*radint)
     profint = gearint.get_profile()
     profint[0] += radext - radint
-    plt.plot(profint[0], profint[1])
+    plt.plot(*profint)
 
     ax.set_aspect('equal')
+    ax.set_title("Cycloidal")
     ax.add_patch(get_primext())
     ax.add_patch(get_primint())
 
 
-    # Fourth gear type: elliptic
+    # Fifth gear type: elliptical involute
     get_primint = lambda a,b: pat.Ellipse(
         (radext - a, 0), 2*a, 2*b, color='red', linestyle='dashed', fill=False)
-    # Compute the ellipse parameters s.t. its perimeter is equivalent to a
-    # circle of radius 'radint'.
+
     ecc = 0.6
     ellip = Ellipse2(radint, ecc**2)
     semimajor, semiminor = ellip.a, ellip.b
 
-    ax = plt.subplot(224)
-#    ax = plt.subplot(111)
-    gearext = Sinusoidal(radext, radial_pitch*radext, tooth_radius=0.1)
+    ax = plt.subplot(234)
+    gearext = gear.InvoluteElliptical(radext, 0., radial_pitch*radext,
+                                      internal=True)
     profext = gearext.get_profile()
-    plt.plot(profext[0], profext[1])
+    plt.plot(*profext)
 
-    gearint = SinusoidalElliptic(semimajor, semiminor, radial_pitch*radint,
-                                 tooth_radius=0.1)
+    gearint = gear.InvoluteElliptical(radint, ecc**2, radial_pitch*radint)
     profint = gearint.get_profile()
     profint[0] += radext - semimajor
-    plt.plot(profint[0], profint[1])
+    plt.plot(*profint)
 
     ax.set_aspect('equal')
+    ax.set_title("Elliptical involute")
+    ax.add_patch(get_primext())
+    ax.add_patch(get_primint(semimajor, semiminor))
+
+
+    # Fourth gear type: elliptical sinusoidal
+    ax = plt.subplot(235)
+    gearext = gear.Sinusoidal(radext, radial_pitch*radext, tooth_radius=0.1)
+    profext = gearext.get_profile()
+    plt.plot(*profext)
+
+    gearint = gear.SinusoidalElliptical(
+        semimajor, semiminor, radial_pitch*radint, tooth_radius=0.1)
+    profint = gearint.get_profile()
+    profint[0] += radext - semimajor
+    plt.plot(*profint)
+
+    ax.set_aspect('equal')
+    ax.set_title("Elliptical sinusoidal")
     ax.add_patch(get_primext())
     ax.add_patch(get_primint(semimajor, semiminor))
 
 
 
 
+    plt.gcf().canvas.set_window_title("Gear profiles")
     plt.ioff()
     plt.show()
 
--- gearprofile.py
+++ gearprofile.py
@@ -6,12 +6,13 @@ Module for creating involute spur gears.
 """
 import math
 import numpy as np
+import scipy.optimize as opt
 
 import curves as cu
 
 
 class GearProfile:
-    points_per_tooth = 20
+    points_per_tooth = 2**4
 
 class Involute(GearProfile):
     """Circular involute spur gear profile.
@@ -181,7 +182,7 @@ class Sinusoidal(GearProfile):
         N = self.nb_teeth
         r = self.tooth_radius
         t = np.linspace(0, 2 * math.pi, GearProfile.points_per_tooth * N)
-        # Note: the actual formula for the profile is 
+        # Note: the actual formula for the profile is
         # r*cos(2*pi*N*arclength(t)/perimeter) = r*cos(2*pi*N*r*t/(2*pi*r))
         #                                      = r*cos(N*t)
         return (R + r * np.cos(N * t)) * np.vstack([np.cos(t), np.sin(t)])
@@ -219,7 +220,7 @@ class Cycloidal(GearProfile):
         # Compute the arguments.
         period_angle = 2 * math.pi / self.nb_teeth
         te = np.linspace(0, period_angle / 2, GearProfile.points_per_tooth)
-        th = np.linspace(period_angle / 2, period_angle, 
+        th = np.linspace(period_angle / 2, period_angle,
                          GearProfile.points_per_tooth)
         # Compute the tooth.
         tooth = np.hstack([epi.get_point(te)[:, :-1], hypo.get_point(th)])
@@ -232,8 +233,169 @@ class Cycloidal(GearProfile):
         return rot.dot(tooth)
 
 
-class SinusoidalElliptic(GearProfile):
-    """Sinusoidal elliptic gear profile."""
+class InvoluteElliptical(GearProfile):
+    """Elliptical involute spur gear profile."""
+
+    def __init__(self, pitch_req, pitch_e2, nb_teeth, pressure_angle=20,
+                 internal=False):
+        self.req = pitch_req
+        self.e2 = pitch_e2
+        self.nb_teeth = nb_teeth
+        self.phi_n = pressure_angle * math.pi / 180
+        self.internal = internal
+
+        self.pitch = cu.Ellipse2(self.req, self.e2)
+        m = 2 * self.req / nb_teeth # module
+        self.A0 = m * (1, 2)[self.internal] # Addendum
+        self.B0 = m * math.pi / 4
+        # Radius of the shaper cutter
+        # (Arbitrarily chosen, but seems to work for number of teeth >= 20.)
+        self.rs = m * 12
+
+        self.cos_n = math.cos(self.phi_n)
+        self.sin_n = math.sin(self.phi_n)
+        self.tan_n = self.sin_n / self.cos_n
+
+        self.rho_f = m / 2  # radius of curvature of the fillet
+        self.theta_f = ((math.pi/2) - self.phi_n) # angular spread of the filet
+        self.cos_f = math.cos(self.theta_f)
+        self.sin_f = math.sin(self.theta_f)
+
+    def get_corner_ctr(self, phi_c, sgn):
+        """Get the center of the rounded corner of the shaper cutter at current
+        position.
+        """
+        x = sgn*(self.B0 + self.A0*self.tan_n + self.rho_f*self.sin_f) + self.rs*phi_c
+        y = -self.A0 + self.rho_f*self.cos_f + self.rs
+        return x, y
+
+    def get_lp(self, phi_c, sgn):
+        """Get lp, an intermediary parameter."""
+        return -(sgn*self.rs*phi_c + self.B0)*self.sin_n - self.A0/self.cos_n
+
+    def get_work_pt(self, phi_c, sgn):
+        """Get the coordinates of the working point in the local ref. of the
+        cutting rack.
+        """
+        lp = self.get_lp(phi_c, sgn)
+        x = sgn * (self.B0 + self.A0*self.tan_n - lp*self.sin_n) + self.rs*phi_c
+        y = lp*self.cos_n - self.A0 + self.rs
+        return x, y
+
+    def get_meshing_eq(self, phi_c, phi_s, sgn):
+        """Get the value of the equation of meshing between the cutter and the
+        gear.
+        A value of 0 means that the two elements are meshing correctly.
+        """
+        A1, B1 = self.get_work_pt(phi_c, sgn)
+        left = (sgn*self.rs) * np.cos(self.phi_n - sgn * (phi_s - phi_c))
+        right = (sgn*B1*self.cos_n - A1*self.sin_n)
+        return left - right
+
+#def get_phi_s_bounds(sgn):
+#    phi_c_bnds = -sgn * (B0 + np.array([1, 3])*A0 / (cos_n*sin_n)) / rs
+#    return np.array([
+#        opt.fsolve(lambda x: get_meshing_eq(val, x, sgn), 0.)[0]
+#        for val in phi_c_bnds]) * rs
+##    t_bnds = ellipse.get_arclength_inv(s_bnds)
+##    phi_bnds = np.arctan2(np.sin(t_bnds) * math.sqrt(1 - e**2), np.cos(t_bnds))
+##    print(phi_bnds)
+
+    def get_profile_side(self, sgn):
+        """Returns working regions from one side of each tooth, and fillets
+        from the opposite side.
+         -- sgn > 0: left-side working regions, right-side fillets,
+         -- sgn < 0: right-side working regions, left-side fillets.
+        """
+        sgn = int(sgn)
+        working = []
+        fillet = []
+
+        for tid in range(self.nb_teeth):
+            # phi_s
+            s = np.linspace(
+                2*math.pi*self.req*tid/self.nb_teeth,
+                2*math.pi*self.req*(tid+1)/self.nb_teeth,
+                GearProfile.points_per_tooth)
+            phi_s = (s - s[(-1, 0)[sgn > 0]]) / self.rs
+            # phi
+            t = self.pitch.get_arclength_inv(s)
+            phi = np.arctan2(np.sin(t) * math.sqrt(1 - self.e2), np.cos(t))
+            cos_phi = np.cos(phi)
+            sin_phi = np.sin(phi)
+            # gamma
+            den = np.sqrt(1 + cos_phi**2 * self.e2 * (self.e2 - 2))
+            cos_gamma = sin_phi / den
+            sin_gamma = cos_phi*(1 - self.e2) / den
+            gamma = np.arctan2(sin_gamma, cos_gamma)
+            # phi_c
+            phi_c = opt.fsolve(
+                lambda x: self.get_meshing_eq(x, phi_s, sgn), phi_s)
+
+            r = np.linalg.norm(self.pitch.get_point(t), axis=0)
+            cos_ = np.cos(phi_s - gamma - phi_c)
+            sin_ = np.sin(phi_s - gamma - phi_c)
+
+            # Working point in rack ref
+            xy = self.get_work_pt(phi_c, sgn)
+            # Working point in fixed ref
+            xy = np.vstack(
+                [ xy[0]*cos_ + xy[1]*sin_ + r*cos_phi + self.rs*sin_gamma,
+                 -xy[0]*sin_ + xy[1]*cos_ - r*sin_phi - self.rs*cos_gamma]
+                )
+
+    #        lp = self.get_lp(phi_c, sgn)
+    #        valid = np.logical_and(lp >= 0., lp <= 2*self.A0/self.cos_n)
+    #        working.append(xy[:, valid])
+            working.append(xy)
+
+            if not self.internal:
+                # Corner center in rack ref
+                xy = self.get_corner_ctr(phi_c, -sgn)
+                # Corner center in fixed ref = Primary trochoid
+                # (We reverse it because if we go up the tooth on one side,
+                # we will go down the fillet on the other, and vice-versa.)
+                xy = np.vstack(
+                    [ xy[0]*cos_ + xy[1]*sin_ + r*cos_phi + self.rs*sin_gamma,
+                     -xy[0]*sin_ + xy[1]*cos_ - r*sin_phi - self.rs*cos_gamma]
+                    )[:, ::-1]
+                # Secondary trochoid
+                xy_prime = np.vstack([np.gradient(xy[0]), np.gradient(xy[1])])
+                norm = np.sqrt(xy_prime[0]**2 + xy_prime[1]**2)
+                xy_prime /= norm
+                xy[0] += self.rho_f * xy_prime[1]
+                xy[1] -= self.rho_f * xy_prime[0]
+                # Filter values.
+                ref = xy_prime[:, (-1, 0)[sgn > 0]]
+                cosines = ref[0]*xy_prime[0, :] + ref[1]*xy_prime[1, :]
+                xy = xy[:, cosines > 0]
+
+                fillet.append(xy)
+
+        if sgn > 0:
+            working = working[1:] + working[:1]
+
+        return working, fillet
+
+    def get_profile(self):
+        """Create a circular gear profile."""
+        working_l, fillet_r = self.get_profile_side(1)
+        working_r, fillet_l = self.get_profile_side(-1)
+        working = [np.hstack([l, r]) for l, r in zip(working_l, working_r)]
+        if not self.internal:
+            fillets = [np.hstack([r, l])
+                       for r, l in zip(fillet_r, fillet_l)]
+            profile = [np.hstack([f, w]) for f, w in zip(fillets, working)]
+        else:
+            profile = working
+        profile = np.hstack(profile)
+        profile = np.hstack([profile, profile[:, [0]]])
+
+        return profile
+
+
+class SinusoidalElliptical(GearProfile):
+    """Sinusoidal elliptical gear profile."""
 
     def __init__(self, pitch_semimajor, pitch_semiminor, nb_teeth, tooth_radius):
         self.pitch_semimajor = pitch_semimajor
@@ -242,7 +404,7 @@ class SinusoidalElliptic(GearProfile):
         self.tooth_radius = tooth_radius
 
     def get_profile(self):
-        """Create an elliptic gear profile."""
+        """Create an elliptical gear profile."""
         shape = cu.Ellipse(self.pitch_semimajor, self.pitch_semiminor)
         N = self.nb_teeth
         r = self.tooth_radius
