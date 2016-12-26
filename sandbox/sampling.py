977824eeac42d16dbbd29697feb84c15a05b5546
Robin Roussel <robin.roussel@inria.fr>
2016-12-26 09:33:23 +0100

    Expand space-sampling test script.

--- sandbox/sampling.py
+++ sandbox/sampling.py
@@ -5,12 +5,26 @@ Sampling the design space.
 
 @author: Robin Roussel
 """
+import time
 import numpy as np
 import context
-from mecha import SingleGearFixedFulcrumCDM as CDM
+
+if(0):
+    from mecha import SingleGearFixedFulcrumCDM as TYPE
+    filename = "cdm_dom.npy"
+else:
+    from mecha import HootNanny as TYPE
+    filename = "hoot_dom.npy"
+    # max_nb_turns = 3, grid_resol = 4; Elapsed time: 135s
+
+#import warnings
+#warnings.filterwarnings("error")
 
 def fun():
-    dom = list(CDM.ConstraintSolver.sample_feasible_domain())
+    t1 = time.time()
+    dom = list(TYPE.ConstraintSolver.sample_feasible_domain())
+    t2 = time.time()
+    print("Elapsed time: {}s".format(t2-t1))
     return np.array(dom)
 
 def load(filename):
@@ -18,18 +32,53 @@ def load(filename):
     return dom
 
 if __name__ == "__main__":
-    dom = fun()
-    print("Number of samples: ", dom.shape[0])
-    constraints = CDM.ConstraintSolver.get_constraints()
-    if all((cons(dom.T) >= 0).all() for cons in constraints):
-        print("All samples satisfy the constraints.")
-    unique = np.vstack({tuple(row) for row in dom})
-    if unique.shape == dom.shape:
-        print("All samples are unique.")
+    if(1):
+        dom = fun()
+        print("Number of samples: ", dom.shape[0])
+        constraints = TYPE.ConstraintSolver.get_constraints()
+        valid = np.array([(cons(dom.T) >= 0).all() for cons in constraints])
+        if all(valid):
+            print("All samples satisfy the constraints.")
+        else:
+            print("The following constraints are not satisfied: {}".format(
+                  np.where(~valid)[0]))
+        unique = np.vstack({tuple(row) for row in dom})
+        if unique.shape == dom.shape:
+            print("All samples are unique.")
+        else:
+            print("There are {} duplicate samples.".format(
+                  dom.shape[0] - unique.shape[0]))
 
-    filename = "cdm_dom.npy"
-    np.save(filename, dom)
+        np.save(filename, dom)
+        dom2 = load(filename)
+        if np.allclose(dom, dom2):
+            print("Samples were successfully saved.")
 
-    dom2 = load(filename)
-    if np.allclose(dom, dom2):
-        print("Samples were successfully saved.")
+    else:
+        dom = load(filename)
+        print(dom.shape)
+        constraints = TYPE.ConstraintSolver.get_constraints()
+        valid = np.array([(cons(dom.T) >= 0).all() for cons in constraints])
+        invalid = np.where(~valid)[0]
+        print(invalid)
+
+        for id_ in invalid:
+            print("Constraint no. {}".format(id_))
+            values = constraints[id_](dom.T)
+            print("Invalid samples: {}".format(np.where(values < 0)[0]))
+            print("Max constraint violation: {}".format(min(values)))
+
+        unique = np.vstack({tuple(row) for row in dom})
+        if unique.shape == dom.shape:
+            print("All samples are unique.")
+        else:
+            print("There are {} duplicate samples.".format(
+                  dom.shape[0] - unique.shape[0]))
+            samples = [tuple(row) for row in dom]
+#            doublons = {}
+            for i, sample in enumerate(samples):
+                nb = samples.count(sample)
+                if nb > 1:
+                    print(i, sample)
+#                    doublons[sample] = i
+#            print(doublons)
