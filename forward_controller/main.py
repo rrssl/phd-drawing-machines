a83de6af00ec5da2aba5f2093c5c262f3be0aba6
Robin Roussel <robin.roussel.15@ucl.ac.uk>
2018-05-13 01:05:31 +0100

    Add CLI parameters to the forward controller

--- forward_controller/main.py
+++ forward_controller/main.py
@@ -1,40 +1,56 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 App allowing direct exploration of the feasible design space of a machine.
 
-@author: Robin Roussel
+
+Parameters
+----------
+mid : int
+  Mechanism.
+pt_density : int, optional
+  Density of the curve.
+
 """
-import _context
+import sys
+
+import _context  # noqa
 from _base import ForwardController
 
 
-pt_density = 2**6
-if 0:
-    from mecha import BaseSpirograph as mecha_type
-    from _config import spiro_data as data
-elif 0:
-    from mecha import EllipticSpirograph as mecha_type
-    from _config import ellip_data as data
-elif 0:
-    from mecha import SingleGearFixedFulcrumCDM as mecha_type
-    from _config import cdm_data as data
-elif 0:
-    from mecha import HootNanny as mecha_type
-    from _config import hoot_data as data
-    pt_density = 2**10
-elif 1:
-    from mecha import Kicker as mecha_type
-    from _config import kick_data as data
-elif 0:
-    from mecha import Thing as mecha_type
-    from _config import thing_data as data
-    pt_density = 2**8
-
-
 def main():
+    if len(sys.argv) < 2:
+        print("Please choose a mechanism")
+        return
+    mid = int(sys.argv[1])
+    if len(sys.argv) < 3:
+        pt_density = 2**6
+    else:
+        pt_density = int(sys.argv[2])
+
+    if mid == 0:
+        from mecha import BaseSpirograph as mecha_type
+        from _config import spiro_data as data
+    elif mid == 1:
+        from mecha import EllipticSpirograph as mecha_type
+        from _config import ellip_data as data
+    elif mid == 2:
+        from mecha import SingleGearFixedFulcrumCDM as mecha_type
+        from _config import cdm_data as data
+    elif mid == 3:
+        from mecha import HootNanny as mecha_type
+        from _config import hoot_data as data
+    elif mid == 4:
+        from mecha import Kicker as mecha_type
+        from _config import kick_data as data
+    elif mid == 5:
+        from mecha import Thing as mecha_type
+        from _config import thing_data as data
+    else:
+        print("Invalid mechanism")
+        return
+
     app = ForwardController(mecha_type, data, pt_density)
     app.run()
+
 
 if __name__ == "__main__":
     main()
